e3d7c6f4128a702644a8b4e1e8330bed1c5c8326
Jose Aviles <jlapmail@gmail.com>
2019-06-20 08:06:26 -0400

    Add production command

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,61 @@
 const path = require('path');
 
 module.exports = [{
-  entry: './src/rendering/index.tsx',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {},
-          },
-        ],
-      },
-    ]
-  },
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ]
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist', 'web')
-  }
+    entry: './src/rendering/index.tsx',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {}
+                    }
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist', 'web')
+    }
 },
 {
-  entry: './src/server.ts',
-  target: "node",
-  node: {
-    __dirname: false,
-    __filename: false,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      },
-    ]
-  },
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
-  output: {
-    filename: 'server.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    entry: './src/server.ts',
+    target: 'node',
+    node: {
+        __dirname: false,
+        __filename: false
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    output: {
+        filename: 'server.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    stats: {
+        warningsFilter: warning => {
+            // Critical dependency
+            return RegExp('node_modules/express/lib/view.js').test(warning);
+        }
+    }
 }
 ];
